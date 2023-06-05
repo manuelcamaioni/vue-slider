47631d6a43bb89c74951c500fb4016a199b42072
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-05 14:02:11 +0200

    imported imgs + declared array

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "heloololooloool",
+            images: [
+                "./img/01.webp",
+                "./img/02.webp",
+                "./img/03.webp",
+                "./img/04.webp",
+                "./img/05.webp",
+            ],
         };
     },
 }).mount("#app");
